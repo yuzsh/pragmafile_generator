fe88ed0adbb84304fbe31a7c967da52195543ec0
yuzsh <yuzatakujp1013@gmail.com>
2016-09-26 17:01:55 +0900

    release candidate

--- pragma_generator.py
+++ pragma_generator.py
@@ -11,15 +11,13 @@ def fetch_libname(path):
 
     for file in files:
         names = os.path.basename(file)
-        lib_name.append(names.split("\n"))
+        lib_name += names.split("\n")
 
     return lib_name
 
 def main():
-    # file_name = sys.argv[1] if sys.argv[1] else "hogehoge"
-    # path = sys.argv[2] if sys.argv[2] else os.getcwd()
-    file_name = "hogehoge"
-    path = "D:\\build\\bin_vtk\\lib\\Debug"
+    file_name = sys.argv[1] if sys.argv[1] else "hogehoge"
+    path = sys.argv[2] if sys.argv[2] else os.getcwd()
     lib_names = fetch_libname(path)
 
 
@@ -30,19 +28,17 @@ def main():
     for name in lib_names:
         debug_libs = []
         release_libs = []
-        if str(name).rfind("d.lib") == -1: # match release files
-            release_libs = name
-            debug_libs.append(str(name).replace(".lib", "d.lib"))
+        if name.rfind("d.lib") == -1: # match release files
+            release_libs.append(name)
+            debug_libs.append(name.replace(".lib", "d.lib"))
         else: # match debug files
-            debug_libs = name
-            release_libs.append(str(name).replace("d.lib", ".lib"))
+            debug_libs.append(name)
+            release_libs.append(name.replace("d.lib", ".lib"))
 
         char_name_debug = ','.join(debug_libs)
         pragma_char_debug += "#pragma comment(lib, '" + char_name_debug + "')\n"
         char_name_release = ','.join(release_libs)
-        # pragma_char_release += "#pragma comment(lib, '" + char_name_release + "')\n"
-        pragma_char_release += "#pragma comment(lib, '" + char_name_debug + "')\n"
-        # print(pragma_char)
+        pragma_char_release += "#pragma comment(lib, '" + char_name_release + "')\n"
 
     try:
         f = open(file_name + ".hpp", "w")
